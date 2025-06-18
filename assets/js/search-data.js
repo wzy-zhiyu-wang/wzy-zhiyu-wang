f81c5bb0d19bf3b0fdc4c4bf0227f1a3df50670c
wzy-zhiyu-wang <zhiyu_wang_work@outlook.com>
2025-06-18 15:51:34 +0000

    Deploying to gh-pages from @ wzy-zhiyu-wang/wzy-zhiyu-wang.github.io@099a9a15cf2190a70f4bc3eb8cf280604e948ec6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "You can download the latest version of my resume/CV using the PDF download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
